2f00d14255377ba60495dbc752a5ed9d981e8736
Tristan MOREAU <toreau@keemail.me>
2022-05-16 19:16:05 +0200

    update

---
+++ custom/setup-lsp-js.el
@@ -0,0 +1,15 @@
+
+(add-hook 'prog-mode-hook #'lsp)
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      company-idle-delay 0.0
+      company-minimum-prefix-length 1
+      create-lockfiles nil) ;; lock files will kill `npm start'
+(with-eval-after-load 'lsp-mode
+  (require 'dap-chrome)
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+  (yas-global-mode))
+(with-eval-after-load 'js
+  (define-key js-mode-map (kbd "M-.") nil))
+
+(provide 'setup-lsp-js)
--- init.el
+++ init.el
@@ -1,7 +1,10 @@
 ;;melpa
 (require 'package)
-
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; (add-to-list 'package-archives
+;;              '("melpa-stable" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+;; (add-to-list 'package-archives
+;;              '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 (when (not package-archive-contents)
@@ -13,7 +16,7 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(setq package-selected-packages '(lsp-mode lsp-ui lsp-pyright projectile flycheck company avy which-key helm-xref dap-mode json-mode realgud))
+(setq package-selected-packages '(lsp-mode lsp-ui lsp-treemacs helm-lsp lsp-pyright projectile hydra flycheck company avy which-key helm-xref dap-mode json-mode realgud ein))
 (when (cl-find-if-not #'package-installed-p package-selected-packages)
   (package-refresh-contents)
   (mapc #'package-install package-selected-packages))
@@ -24,28 +27,21 @@
 (add-to-list 'load-path "~/.emacs.d/custom")
 (require 'setup-general)
 (require 'setup-helm)
+;(require 'setup-helm2)
 (require 'setup-editing)
 (require 'setup-projectile)
-(require 'setup-python)
-(require 'setup-lsp-cpp)
-(require 'setup-debugger)
+;(require 'setup-python)
+;(require 'setup-lsp-cpp)
+(require 'setup-lsp-js)
+;(require 'setup-debugger)
 
-;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(package-selected-packages
-;;    '(ein docker-compose-mode dockerfile-mode lsp-pyright lsp-jedi cmake-mode anzu volatile-highlights paredit lsp-ui lsp-mode yasnippet lsp-treemacs helm-lsp projectile hydra flycheck company avy which-key helm-xref dap-mode zenburn-theme json-mode)))
-;; (custom-set-faces
-;;  )
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(docker-compose-mode docker ein emmet-mode realgud lsp-mode lsp-ui lsp-pyright projectile flycheck company avy which-key helm-xref dap-mode json-mode)))
+   '(js2-mode virtualenv jupyter web-mode lsp-mode lsp-ui lsp-treemacs helm-lsp lsp-pyright projectile hydra flycheck company avy which-key helm-xref dap-mode json-mode realgud)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
